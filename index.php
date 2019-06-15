05702e467a46555c371cdee9ba38143f0639b3e0
Florencia Sangineto <florenciasangineto@gmail.com>
2019-06-15 17:08:30 -0300

    home

--- index.php
+++ index.php
@@ -87,36 +87,26 @@
 
    <div class="row featurette">
      <div class="col-md-7">
-       <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It’ll blow your mind.</span></h2>
+       <h2 class="featurette-heading">Recetas organicas.<br> <span class="text-muted">Para esta época.</span></h2>
        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+       <button type="button" class="btn btn-info ">Conocé Más</button>
      </div>
      <div class="col-md-5">
-       <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
+       <img src="images/recetas/pie.jpg" alt="">
      </div>
    </div>
 
    <hr class="featurette-divider">
 
    <div class="row featurette">
-     <div class="col-md-7 order-md-2">
-       <h2 class="featurette-heading">Oh yeah, it’s that good. <span class="text-muted">See for yourself.</span></h2>
-       <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+     <div class="col-md-5">
+       <img src="images/nosotros.jpg" alt="">
      </div>
-     <div class="col-md-5 order-md-1">
-       <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-     </div>
-   </div>
-
-   <hr class="featurette-divider">
-
-   <div class="row featurette">
      <div class="col-md-7">
-       <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+       <h2 class="featurette-heading">Elegimos lo mejor. <span class="text-muted">Tenemos lo mejor.</span></h2>
        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
      </div>
-     <div class="col-md-5">
-       <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-     </div>
+
    </div>
 
    <hr class="featurette-divider">
@@ -128,5 +118,3 @@
 
 
 <?php require "includes/footer.php"; ?>
-
- 
