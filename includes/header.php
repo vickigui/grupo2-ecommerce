01d5825707bd169c1ae79ecb769b3c53034a5db6
Florencia Sangineto <florenciasangineto@gmail.com>
2019-06-15 16:55:05 -0300

    Subo header y footer

---
+++ includes/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>PALTA - 100% Productos Orgánicos</title>
+
+    <link rel="shortcut icon" href="images/common/favicon.ico" type="image/x-icon">
+
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700,800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles/common.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link href="styles/fontawesome/css/all.css" rel="stylesheet">
+
+
+  </head>
+
+
+
+  <body>
+    <header>
+ <nav class="navbar navbar-expand-md navbar-light fixed-top bg-white">
+   <a class="navbar-brand" href="#"><img src="images/common/logo-palta.png" width=80px;></a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="navbarCollapse">
+     <ul class="navbar-nav mr-auto">
+       <li class="nav-item active">
+         <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">NOSOTROS</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">PRODUCTOS</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">ALMACÉN</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">PROMOS</a>
+       </li>
+       <li class="nav-item">
+         <a class="nav-link" href="#">RECETAS</a>
+       </li>
+
+
+     </ul>
+     <form class="form-inline mt-2 mt-md-0">
+       <input class="form-control mr-sm-2" type="text" placeholder="Buscá tu producto" aria-label="Search">
+       <button class="btn btn-success my-2 my-sm-0" type="submit">Buscar</button>
+     </form>
+
+     <div class="user"><i class="fas fa-user"></i></div>
+     <div class="carrito"><i class="fas fa-shopping-cart"></i></div>
+   </div>
+ </nav>
+</header>
--- index.php
+++ index.php
@@ -1,56 +1,4 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>PALTA - 100% Productos Orgánicos</title>
-
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700,800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styles/common.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-
-  </head>
-
-
-
-  <body>
-    <header>
- <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-yellow">
-   <a class="navbar-brand" href="#">PALTA</a>
-   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-     <span class="navbar-toggler-icon"></span>
-   </button>
-   <div class="collapse navbar-collapse" id="navbarCollapse">
-     <ul class="navbar-nav mr-auto">
-       <li class="nav-item active">
-         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-       </li>
-       <li class="nav-item">
-         <a class="nav-link" href="#">Frutas</a>
-       </li>
-       <li class="nav-item">
-         <a class="nav-link" href="#">Verduras</a>
-       </li>
-       <li class="nav-item">
-         <a class="nav-link" href="#">Extras</a>
-       </li>
-       <li class="nav-item">
-         <a class="nav-link" href="#">Canastas</a>
-       </li>
-       <li class="nav-item">
-         <a class="nav-link" href="#">Recetas</a>
-       </li>
-
-
-     </ul>
-     <form class="form-inline mt-2 mt-md-0">
-       <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-     </form>
-   </div>
- </nav>
-</header>
+<?php require "includes/header.php"; ?>
 
 <main role="main">
 
@@ -176,20 +124,9 @@
    <!-- /END THE FEATURETTES -->
 
  </div><!-- /.container -->
-
-
- <!-- FOOTER -->
- <footer class="container">
-   <p class="float-right"><a href="#">Back to top</a></p>
-   <p>&copy; 2017-2019 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
- </footer>
 </main>
 
 
-  </body>
+<?php require "includes/footer.php"; ?>
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-</html>
+ 
