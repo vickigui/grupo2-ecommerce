e14fc1c9a89518dc5eedad7665f072e3ce56cc53
Victoria <vikie.g@hotmail.com>
2019-06-17 15:05:42 -0300

    cambios en el form

---
+++ contacto.php
@@ -0,0 +1,31 @@
+    <?php require "includes/header.php"; ?>
+
+      <div class="container form col-xs-8 col-lg-5 formSection">
+        <h1>¡ CONTACTANOS !</h1>
+        <h4>Dejanos tu consulta y nos pondremos en contacto con vos</h4>
+        <form action="index.php" method="post">
+          <label for="name" id="name" class="items">
+            <p>Nombre y apellido</p>
+          </label>
+          <input type="text" name="name" value="">
+
+          <label for="mail" id="mail" class="items">
+            <p>E-mail</p>
+          </label>
+          <input type="mail" name="mail" value="">
+
+          <label for="telephone" id="telephone" class="items">
+            <p>Teléfono</p>
+          </label>
+          <input type="tel" name="telephone" value="">
+
+          <label for="contactMessage" id="contactMessage" class="contactMessage">
+            <p>Dejanos tu consulta</p>
+          </label>
+          <textarea name="contactMessage" rows="8"></textarea>
+
+          <button type="submit" name="button" class="btn btn-success btn-form">Enviar</button>
+        </form>
+      </div>
+
+    <?php require "includes/footer.php"; ?>
---
+++ registro.php
@@ -0,0 +1,31 @@
+<?php require "includes/header.php"; ?>
+
+  <div class="container form col-xs-8 col-lg-5 formSection">
+    <h1>¡ REGISTRATE !</h1>
+    <h4>Completá los siguientes datos para empezar.</h4>
+    <form action="index.php" method="post">
+      <label for="name" id="name" class="items">
+        <p>Nombre y apellido</p>
+      </label>
+      <input type="text" name="name" value="">
+
+      <label for="mail" id="mail" class="items">
+        <p>E-mail</p>
+      </label>
+      <input type="mail" name="mail" value="">
+
+      <label for="password" id="password" class="items">
+        <p>Contraseña</p>
+      </label>
+      <input type="password" name="password" value="">
+
+      <label for="password" id="password" class="items">
+        <p>Repetir contraseña</p>
+      </label>
+      <input type="password" name="password" value="">
+
+      <button type="submit" name="button" class="btn btn-success btn-form">Enviar</button>
+    </form>
+  </div>
+
+<?php require "includes/footer.php"; ?>
