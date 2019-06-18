652da27b5c52aaffe3504341f23e283f70283080
Florencia Sangineto <florenciasangineto@gmail.com>
2019-06-18 16:53:47 -0300

    aplico header y footer en productos

--- productos.php
+++ productos.php
@@ -1,5 +1,94 @@
 <?php require "includes/header.php"; ?>
 
-
+<main>
+  <div class="container">
+    <h1>
+      Nuestros Productos
+    </h1>
+    <h2>
+      <a href="#verduras"> Verduras</a>
+      <br>
+      <a href="#frutas">Frutas</a>
+      <br>
+      <a href="#Almacen">Almacen orgánico</a>
+    </h2>
+    <ul>
+      <li>
+        <article class="">
+          Imagen del producto
+          <a href="#">
+            <p> Producto </p>
+            Descripción/Precio por cantidad
+          </a>
+        </article>
+      </li>
+      <br>
+      <li>
+        <article class="">
+          Imagen del producto
+          <a href="#">
+            <p> Producto </p>
+            Descripción/Precio por cantidad
+          </a>
+          <br>
+          <button type="submit" name="button">Eliminar de mi carrito de compras.</button>
+          <button type="button" name="button">Añadir al carrito de compras.</button>
+        </article>
+        </li>
+        <br>
+        <li>
+          <article class="">
+            Imagen del producto
+            <a href="#">
+              <p> Producto </p>
+              Descripción/Precio por cantidad
+            </a>
+            <br>
+            <button type="submit" name="button">Eliminar de mi carrito de compras.</button>
+            <button type="button" name="button">Añadir al carrito de compras.</button>
+          </article>
+        </li>
+        <br>
+        <li>
+        <article class="">
+          Imagen del producto
+          <a href="#">
+            <p> Producto </p>
+            Descripción/Precio por cantidad
+          </a>
+           <br>
+          <button type="submit" name="button">Eliminar de mi carrito de compras.</button>
+          <button type="button" name="button">Añadir al carrito de compras.</button>
+        </article>
+        </li>
+        <br>
+        <li>
+          <article class="">
+            Imagen del producto
+            <a href="#">
+              <p> Producto </p>
+              Descripción/Precio por cantidad
+            </a>
+            <br>
+            <button type="submit" name="button">Eliminar de mi carrito de compras.</button>
+            <button type="button" name="button">Añadir al carrito de compras.</button>
+          </article>
+        </li>
+        <br>
+        <li>
+          <article class="">
+            Imagen del producto
+            <a href="#">
+              <p> Producto </p>
+              Descripción/Precio por cantidad
+            </a>
+             <br>
+            <button type="submit" name="button">Eliminar de mi carrito de compras.</button>
+            <button type="button" name="button">Añadir al carrito de compras.</button>
+          </article>
+      </li>
+    </ul>
+  </div>
+</main>
 
 <?php require "includes/footer.php"; ?>
