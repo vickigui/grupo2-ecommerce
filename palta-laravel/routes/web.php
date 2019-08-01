64369caad1cab43803d629b5cb4547dab2ad6241
Florencia Sangineto <florenciasangineto@gmail.com>
2019-08-01 20:59:10 -0300

    palta laravel palta php

---
+++ palta-laravel/routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+Route::get('/registro', function () {
+    return view('registro');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/perfil', function () {
+    return view('perfil');
+});
+
+Route::get('/perfil/carrito', function () {
+    return view('carrito');
+});
+
+Route::get('/productos', function () {
+    return view('productos');
+});
+
+Route::get('/recetas', function () {
+    return view('recetas');
+});
+
+Route::get('/nosotros', function () {
+    return view('nosotros');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
+Route::get('/admin', function () {
+    return view('admin');
+});
