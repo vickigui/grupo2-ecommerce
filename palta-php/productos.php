ff802f48ec8ff2cc54b829929bf984f399c9b0de
Florencia Sangineto <florenciasangineto@gmail.com>
2019-09-03 21:04:56 -0300

    productos

--- palta-php/productos.php
+++ palta-php/productos.php
@@ -1,9 +1,13 @@
-<?php require "includes/header.php";
+<?php
+require_once "database/db.php";
+require_once "database/productos.php";
+
+$productos= allproducts($db);
 
 
 ?>
 
-
+<?php require "includes/header.php"; ?>
   <div class="container-fluid sectionHeader">
     <h1 class="text-center">Nuestros productos</h1>
   </div>
@@ -13,228 +17,37 @@
   		<div class="col-sm-12">
   			<div align="center">
 			    <button class="btn btn-secondary active filter-button" data-filter="todo">Todo</button>
-			    <button class="btn btn-secondary filter-button" data-filter="frutas">Frutas</button>
-			    <button class="btn btn-secondary filter-button " data-filter="verduras">Verduras</button>
+			    <button class="btn btn-secondary filter-button" data-filter="fruta">Frutas</button>
+			    <button class="btn btn-secondary filter-button " data-filter="verdura">Verduras</button>
 			    <button class="btn btn-secondary filter-button" data-filter="almacen">Almacen Orgánico</button>
 
 						<div class="row">
 							<div class="row">
-                <div class="col-lg-3 col-md-4 col-xs-6 thumb filter frutas ">
-                  <img class="img-thumbnail" src="images/productos/canasta1.jpg" alt="Canasta">
-                    <div class="selector-cantidad">
-                      <select class="form-control" id="cantidad">
-                        <option>1</option>
-                         <option>2</option>
-                      </select>
-                        <label>Canasta</label>
-                    </div>
+                <?php foreach ($productos as $row){ ?>
 
-                      <div class="botones-productos">
-                        <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                        <a href="#" class="btn btn-success">Agregar</a>
-                      </div>
-                </div>
-
-                <div class="col-lg-3 col-md-4 col-xs-6 thumb filter frutas ">
-                    <img class="img-thumbnail" src="images/productos/canasta2.jpg" alt="Canasta">
-                        <div class="selector-cantidad">
-                          <select class="form-control" id="cantidad">
-                              <option>1</option>
-                               <option>2</option>
-                           </select>
-                          <label>Canasta</label>
-                        </div>
-                         <div class="botones-productos">
-                           <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                           <a href="#" class="btn btn-success">Agregar</a>
+                         <div class="col-lg-3 col-md-4 col-xs-6 thumb filter <?php echo $row['id_categorias'] ?> ">
+                         <h4><?php echo $row['nombre'] ?></h4>
+                             <img class="img-thumbnail" src="uploads/<?php echo $row['imagen'] ?>" alt="<?php echo $row['nombre'] ?>">
+                                 <div class="selector-cantidad">
+                                   <select class="form-control" id="cantidad">
+                                        <option>0,25</option>
+                                        <option>0,50</option>
+                                        <option>0,75</option>
+                                        <option>1</option>
+                                        <option>2</option>
+                                    </select>
+                                   <label>Kilogramos</label>
+                                 </div>
+                                  <div class="botones-productos">
+                                    <a href="#" class="btn btn-outline-secondary">Quitar</a>
+                                    <a href="#" class="btn btn-success">Agregar</a>
+                                  </div>
                          </div>
-                </div>
-
-                <div class="col-lg-3 col-md-4 col-xs-6 thumb filter frutas ">
-                    <img class="img-thumbnail" src="images/productos/canasta3.jpg" alt="Canasta">
-                        <div class="selector-cantidad">
-                          <select class="form-control" id="cantidad">
-                              <option>1</option>
-                               <option>2</option>
-                           </select>
-                          <label>Canasta</label>
-                        </div>
-                         <div class="botones-productos">
-                           <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                           <a href="#" class="btn btn-success">Agregar</a>
-                         </div>
-                </div>
+                         <?php
+                          }
+                         ?>
 
 
-                      <div class="col-lg-3 col-md-4 col-xs-6 thumb filter frutas ">
-                          <img class="img-thumbnail" src="images/productos/limon.jpg" alt="Limón">
-                              <div class="selector-cantidad">
-                                <select class="form-control" id="cantidad">
-                                     <option>0,25</option>
-                                     <option>0,50</option>
-                                     <option>0,75</option>
-                                     <option>1</option>
-                                     <option>2</option>
-                                 </select>
-                                <label>Kilogramos</label>
-                              </div>
-                               <div class="botones-productos">
-                                 <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                                 <a href="#" class="btn btn-success">Agregar</a>
-                               </div>
-					            </div>
-                      <div class="col-lg-3 col-md-4 col-xs-6 thumb filter frutas ">
-                          <img class="img-thumbnail" src="images/productos/banana.jpg" alt="Banana">
-                              <div class="selector-cantidad">
-                                <select class="form-control" id="cantidad">
-                                     <option>0,25</option>
-                                     <option>0,50</option>
-                                     <option>0,75</option>
-                                     <option>1</option>
-                                     <option>2</option>
-                                 </select>
-                                <label>Kilogramos</label>
-                              </div>
-                               <div class="botones-productos">
-                                 <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                                 <a href="#" class="btn btn-success">Agregar</a>
-                               </div>
-					            </div>
-                      <div class="col-lg-3 col-md-4 col-xs-6 thumb filter frutas ">
-                          <img class="img-thumbnail" src="images/productos/manzana.jpg" alt="Manzana">
-                              <div class="selector-cantidad">
-                                <select class="form-control" id="cantidad">
-                                     <option>0,25</option>
-                                     <option>0,50</option>
-                                     <option>0,75</option>
-                                     <option>1</option>
-                                     <option>2</option>
-                                 </select>
-                                <label>Kilogramos</label>
-                              </div>
-                               <div class="botones-productos">
-                                 <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                                 <a href="#" class="btn btn-success">Agregar</a>
-                               </div>
-					            </div>
-                      <div class="col-lg-3 col-md-4 col-xs-6 thumb filter frutas ">
-                          <img class="img-thumbnail" src="images/productos/mandarina.jpg" alt="Mandarina">
-                              <div class="selector-cantidad">
-                                <select class="form-control" id="cantidad">
-                                     <option>0,25</option>
-                                     <option>0,50</option>
-                                     <option>0,75</option>
-                                     <option>1</option>
-                                     <option>2</option>
-                                 </select>
-                                <label>Kilogramos</label>
-                              </div>
-                               <div class="botones-productos">
-                                 <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                                 <a href="#" class="btn btn-success">Agregar</a>
-                               </div>
-					            </div>
-
-                      <div class="col-lg-3 col-md-4 col-xs-6 thumb filter verduras ">
-                          <img class="img-thumbnail" src="images/productos/lechuga.jpg" alt="Lechuga">
-                              <div class="selector-cantidad">
-                                <select class="form-control" id="cantidad">
-                                     <option>0,25</option>
-                                     <option>0,50</option>
-                                     <option>0,75</option>
-                                     <option>1</option>
-                                     <option>2</option>
-                                 </select>
-                                <label>Kilogramos</label>
-                              </div>
-                               <div class="botones-productos">
-                                 <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                                 <a href="#" class="btn btn-success">Agregar</a>
-                               </div>
-					            </div>
-                      <div class="col-lg-3 col-md-4 col-xs-6 thumb filter verduras ">
-                          <img class="img-thumbnail" src="images/productos/batata.jpg" alt="Batata">
-                              <div class="selector-cantidad">
-                                <select class="form-control" id="cantidad">
-                                     <option>0,25</option>
-                                     <option>0,50</option>
-                                     <option>0,75</option>
-                                     <option>1</option>
-                                     <option>2</option>
-                                 </select>
-                                <label>Kilogramos</label>
-                              </div>
-                               <div class="botones-productos">
-                                 <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                                 <a href="#" class="btn btn-success">Agregar</a>
-                               </div>
-					            </div>
-                      <div class="col-lg-3 col-md-4 col-xs-6 thumb filter verduras ">
-                          <img class="img-thumbnail" src="images/productos/cebolla.jpg" alt="cebolla">
-                              <div class="selector-cantidad">
-                                <select class="form-control" id="cantidad">
-                                     <option>0,25</option>
-                                     <option>0,50</option>
-                                     <option>0,75</option>
-                                     <option>1</option>
-                                     <option>2</option>
-                                 </select>
-                                <label>Kilogramos</label>
-                              </div>
-                               <div class="botones-productos">
-                                 <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                                 <a href="#" class="btn btn-success">Agregar</a>
-                               </div>
-					            </div>
-                      <div class="col-lg-3 col-md-4 col-xs-6 thumb filter verduras ">
-                          <img class="img-thumbnail" src="images/productos/morron.jpg" alt="morron">
-                              <div class="selector-cantidad">
-                                <select class="form-control" id="cantidad">
-                                     <option>0,25</option>
-                                     <option>0,50</option>
-                                     <option>0,75</option>
-                                     <option>1</option>
-                                     <option>2</option>
-                                 </select>
-                                <label>Kilogramos</label>
-                              </div>
-                               <div class="botones-productos">
-                                 <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                                 <a href="#" class="btn btn-success">Agregar</a>
-                               </div>
-					            </div>
-
-                      <div class="col-lg-3 col-md-4 col-xs-6 thumb filter almacen ">
-                          <img class="img-thumbnail" src="images/productos/huevos.jpg" alt="huevos">
-                              <div class="selector-cantidad">
-                                <select class="form-control" id="cantidad">
-                                     <option>1/2</option>
-                                     <option>1</option>
-                                     <option>2</option>
-                                 </select>
-                                <label>Docena</label>
-                              </div>
-                               <div class="botones-productos">
-                                 <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                                 <a href="#" class="btn btn-success">Agregar</a>
-                               </div>
-                      </div>
-                      <div class="col-lg-3 col-md-4 col-xs-6 thumb filter almacen ">
-                          <img class="img-thumbnail" src="images/productos/limon.jpg" alt="Limón">
-                              <div class="selector-cantidad">
-                                <select class="form-control" id="cantidad">
-                                     <option>0,25</option>
-                                     <option>0,50</option>
-                                     <option>0,75</option>
-                                     <option>1</option>
-                                     <option>2</option>
-                                 </select>
-                                <label>Kilogramos</label>
-                              </div>
-                               <div class="botones-productos">
-                                 <a href="#" class="btn btn-outline-secondary">Quitar</a>
-                                 <a href="#" class="btn btn-success">Agregar</a>
-                               </div>
                   </div>
 				        </div>
   						</div>
