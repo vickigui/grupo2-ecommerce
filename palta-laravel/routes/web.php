b7a278123107f75a8662b202839f89b139387bc1
Victoria <vikie.g@hotmail.com>
2019-09-07 15:35:00 -0300

    cambios en user

--- palta-laravel/routes/web.php
+++ palta-laravel/routes/web.php
@@ -38,7 +38,7 @@ Route::get('/faq', function () {
 Route::get('/admin', 'AdminController@admin');
 
 Auth::routes();
-// 
-// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/logout', 'Auth\LoginController@logout');
